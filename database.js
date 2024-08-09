385eee96eb4c2b62ae7090f6f97ff26bb4b3f967
Матвей <neizvestnyjneizvestnost172@gmail.com>
2024-08-09 11:12:00 +0400

    Первый коммит

---
+++ database.js
@@ -0,0 +1,3 @@
+const db = require('mongoose');
+db.connect('mongodb+srv://shadkill:LmM6fm4H0kGa5sY5@cluster0.o0yn5uy.mongodb.net/jongler?retryWrites=true&w=majority&appName=Cluster0');
+module.exports =db;
